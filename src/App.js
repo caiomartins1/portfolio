e1e7a5ebfa13d1cea49245256e9430a5fd316fbe
caiomartins1 <dev.caiomartins@gmail.com>
2021-03-18 22:01:04 +0000

    Setup project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import NavBar from './components/NavBar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,7 @@
+import './index.css';
+
+export default function App() {
+  return (
+    <h2>Nav</h2>
+  );  
+}
