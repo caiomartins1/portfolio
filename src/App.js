f00c012a9292ee0245a7d96618737ddaaf992614
caiomartins1 <dev.caiomartins@gmail.com>
2021-03-18 22:42:16 +0000

    Started developing Navbar component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="app-container">
       <NavBar />
+      {/* Section Components */}
     </div>
   );
 }
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -2,6 +2,16 @@ import './index.css';
 
 export default function App() {
   return (
-    <h2>Nav</h2>
-  );  
+    <header className="navbar-container">
+      <h3 className="navbar-logo"> &lt; CM / &gt;</h3>
+      <nav className="navbar-links-container">
+        <a href="#" className="navbar-link">
+          Test
+        </a>
+        <a href="#" className="navbar-link">
+          Test
+        </a>
+      </nav>
+    </header>
+  );
 }
